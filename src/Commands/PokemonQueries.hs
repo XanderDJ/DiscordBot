a430141ff97e9cfb99e3d7fc4eac7271eee3315c
xanderdj <x.de.j@hotmail.com>
2022-02-28 13:57:20 +0100

    Dt implemented in new query api

--- src/Commands/PokemonQueries.hs
+++ src/Commands/PokemonQueries.hs
@@ -1,10 +1,8 @@
 module Commands.PokemonQueries
   ( module Commands.PokemonQueries.Learn,
-    module Commands.PokemonQueries.DT,
     module Commands.PokemonQueries.PokemonQueries
   )
 where
 
-import Commands.PokemonQueries.DT
 import Commands.PokemonQueries.Learn
 import Commands.PokemonQueries.PokemonQueries
